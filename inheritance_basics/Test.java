a9f943ba51c2838bff1246e52489f8862146fc8a
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2023-04-07 06:11:11 +0530

    basicsThree - using getter and overriding

--- inheritance_basics/Test.java
+++ inheritance_basics/Test.java
@@ -2,6 +2,14 @@ public class Test{
 	public static void main(String[] args){
 		//creating an Object of child class
 
+		System.out.println("Demostrating Overriding");
+		System.out.println("Employee Object");
+		Employee e = new Employee();
+		e.setEmployee(623, "EMP", 100000);
+		e.getEmployee();
+		e.calculateSalary();
+		
+		System.out.println("\nTechEmployee Object");
 		TechEmployee te = new TechEmployee();
 		te.setEmployee(123, "TEMP", 100000);
 		te.getEmployee();
