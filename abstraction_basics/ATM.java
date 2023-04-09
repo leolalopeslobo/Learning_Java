37cca0debfe0dc656114805a44ec1e0afc6a8b87
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2023-04-10 02:28:28 +0530

    specifier

---
+++ abstraction_basics/ATM.java
@@ -0,0 +1,10 @@
+//Specifier
+abstract public class ATM{
+	//abstract methods
+	public abstract void withdraw();
+	public abstract void deposit();
+	//concrete methods
+	public void display(){
+		System.out.println("I'm XYZ ATM");
+	}
+}
