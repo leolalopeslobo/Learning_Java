46181b5c800fa1281eca4dd6af42dc44725b75a4
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2023-07-04 14:42:02 +0530

    parameterized select query

---
+++ AdvancedJavaConcepts/JDBCConnectingJavaProgramtoDataBase/PreparedStatement/PSTDemo2.java
@@ -0,0 +1,48 @@
+import java.sql.*;
+import java.util.Scanner;
+public class PSTDemo2 {
+    public static void main(String[] args) throws Exception{
+        DBConnection dbcon = new DBConnection();
+        Connection con = dbcon.getConnection();
+        
+        if (con!=null)
+            System.out.println("Connection established");
+        else
+            System.out.println("Connection Failed");
+        
+//        Scanner in = new Scanner(System.in);
+//        //Adding a new row by taking the data from user using Prepared Statement
+//        String q1 = "insert into cdac_java_web_programming.student values(?,?,?);";
+//        System.out.print("Enter the sid: ");
+//        int sid = in.nextInt();
+//        System.out.print("Enter the sname: ");
+//        String sname = in.next();
+//        System.out.print("Enter the cgpa: ");
+//        double cgpa = in.nextDouble();
+//        
+//        PreparedStatement pst = con.prepareStatement(q1);
+//        pst.setInt(1, sid);
+//        pst.setString(2, sname);
+//        pst.setDouble(3, cgpa);
+//        
+//        int x = pst.executeUpdate(); //returns int
+//        if(x>0)
+//            System.out.println("Number of records inserted: "+x);
+//        else
+//            System.out.println("Insertion Failed");
+        
+        
+        //retrieving the data you need a result set
+        String q2 = "select * from cdac_java_web_programming.student where sid>=? and sid<=?;";
+        PreparedStatement pst1 = con.prepareStatement(q2);
+        
+        //setting the values
+        pst1.setInt(1,150);
+        pst1.setInt(2, 250);
+        
+        ResultSet rs = pst1.executeQuery();
+        while(rs.next()){
+            System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getDouble(3));
+        }
+    }
+}
